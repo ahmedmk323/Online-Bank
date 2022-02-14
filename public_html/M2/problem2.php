fcb586481f04091cd02f0861d05901f09c917376
Ahmed Mohamed <ahmed.mk2981@gmail.com>
2022-02-13 20:48:29 -0500

    completed hw

--- public_html/M2/problem2.php
+++ public_html/M2/problem2.php
@@ -7,7 +7,11 @@ function getTotal($arr) {
     echo "<br>Processing Array:<br><pre>" . var_export($arr, true) . "</pre>";
     $total = 0.00;
     //TODO do adding here
+    for ($i=0; $i < sizeof($arr); $i++){
+        $total += $arr[$i];
+    }
     //TODO do rounding stuff here
+    $total= round($total);
     echo "The total is " . var_export($total, true);
 }
 echo "Problem 2: Adding Floats<br>";
