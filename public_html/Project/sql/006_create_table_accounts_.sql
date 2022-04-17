a26d887b4c35c58e9f20b4d9f072bd2945c36844
ahmedmk323 <ahmedmk323@users.noreply.github.com>
2022-04-17 18:29:03 -0400

    Create account table

---
+++ public_html/Project/sql/006_create_table_accounts_.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS `Accounts`(
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `account_number` VARCHAR(12) UNIQUE,
+    `user_id` INT,
+    `balance` INT DEFAULT 0,
+    `account_type` VARCHAR(15) NOT NULL,
+    `created` TIMESTAMP  DEFAULT CURRENT_TIMESTAMP,
+    `modified` TIMESTAMP  DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY(`user_id`) REFERENCES Users(`id`)
+)
