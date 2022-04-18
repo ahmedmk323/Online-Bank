33dbe7ea357ac1f08523dc4223ef7ce23bfe57ce
ahmedmk323 <ahmedmk323@users.noreply.github.com>
2022-04-18 00:40:48 -0400

    Added Dashboard and modified NAV

---
+++ public_html/Project/dashboard.php
@@ -0,0 +1,23 @@
+<?php
+require_once(__DIR__ . "/../../partials/nav.php");
+if (!is_logged_in()) {
+    die(header("Location: login.php"));
+}
+?>
+<link rel="stylesheet" href="<?php echo get_url('styles.css'); ?>">
+
+<nav>
+    <ul>
+        <li><a href="#">Create Account</a></li>
+        <li><a href="#">My Accounts</a></li>
+        <li><a href="#">Deposit</a></li>
+        <li><a href="#">Withdraw</a></li>
+        <li><a href="#">Transfer</a></li>
+        <li><a href="<?php echo get_url("profile.php")?>">Profile</a></li>
+    </ul>
+</nav>
+
+<script>
+    const nav= document.getElementsByTagName("nav")[0];
+    nav.remove();
+</script>
