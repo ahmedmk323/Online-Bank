11df8c3bc535342534656ca07c687d0048a874ce
Ahmed Mohamed <ahmed.mk2981@gmail.com>
2022-05-07 21:35:24 -0400

    Pagination Feature

--- public_html/Project/accounts.php
+++ public_html/Project/accounts.php
@@ -10,36 +10,35 @@ if(!$results){
     echo "<center><h2>No accounts are found</h2></center>";
     die;
 }
-echo "<h2>My Accounts</h2>";
 echo"
-<table>
-<thead>
-    <tr>
-        <th>Account Number</th>
-        <th>Balance</th>
-        <th>Type</th>
-        <th>Modified</th>
-    </tr>
-</thead>";
-foreach($results as $key => $value){
-    echo 
-    "<tr>
-        <td id='acc'>$value[account_number]</td>
-        <td>$$value[balance]</td>
-        <td>$value[account_type]</td>
-        <td>$value[modified]</td>
-    </tr>";
-}
-echo "</table>";
-echo "<button onclick=\"transactions()\">View Transactions History</button>";
+<div class=\"container\">
+
+    <center>
+        <h2>My Accounts</h2>
+            <table class=\"table table-striped\">
+                <thead>
+                    <tr>
+                        <th>Account Number</th>
+                        <th>Balance</th>
+                        <th>Type</th>
+                        <th>Modified</th>
+                        <th></th>
+                    </tr>
+                </thead>";
+                foreach($results as $key => $value){
+                    $url= get_url("transactions.php");
+                    $url .= "?account_id=".$value["id"];
+                    echo "<tr>
+                        <td id='acc'>$value[account_number]</td>
+                        <td>$$value[balance]</td>
+                        <td>$value[account_type]</td>
+                        <td>$value[modified]</td>
+                        <td><a href=\"$url\">View Account</a></td>
+                    </tr>";
+                }
+echo "</table> </center> </div>";
 
 ?>
 <div id="Transaction"></div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
-<script>
-    function transactions(){
-    window.location.href= "transactions.php";
-    }
-</script>
--- public_html/Project/transactions.php
+++ public_html/Project/transactions.php
@@ -5,100 +5,123 @@ if (!is_logged_in()) {
     die(header("Location: login.php"));
 }
 
-function getTransactionTable($id){
-    $db= getDB();
-    $query="SELECT *, t2.account_number AS src_acc,
-    t3.account_number AS dest_acc
+//echo "<pre>". se($_GET,null,false,false)."</pre>";
+if(isset($_GET["account_id"])){
+    $id= $_GET["account_id"];
+    $t_query= "SELECT COUNT(1) as `total` FROM Transactions WHERE 1";
+    $d_query="SELECT t2.account_number AS src_acc,
+    t3.account_number AS dest_acc, Transactions.transaction_type, Transactions.balance_change, Transactions.expected_total, Transactions.created, Transactions.memo
     FROM (Transactions
     INNER JOIN Accounts AS t2 ON Transactions.account_src= t2.id 
     INNER JOIN Accounts AS t3 ON Transactions.account_dest = t3.id)
-    WHERE Transactions.account_src= :id
-    ORDER BY Transactions.modified DESC LIMIT 10";
-    $stmt= $db->prepare($query);
-    $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+    WHERE 1";
+    $start= se($_GET,"start",date("Y-m-d", strtotime("-1 month")),false);
+    $end= se($_GET,"end",date("Y-m-d", strtotime("+1 day")),false);
+    $type= se($_GET, "type", false, false);
+    $params= [];
+
+    if($start){
+        $t_query .= " AND Transactions.created >= :start";
+        $d_query .= " AND Transactions.created >= :start";
+        $params[":start"] = $start;
+    }
+
+    if($end){
+        $t_query .= " AND Transactions.created <= :end";
+        $d_query .= " AND Transactions.created <= :end";
+        $params[":end"] = $end;
+    }
+
+    if($type && $type !== "none"){
+        $t_query .= " AND Transactions.transaction_type = :type";
+        $d_query .= " AND Transactions.transaction_type = :type";
+        $params[":type"] = $type; 
+    }
+    $t_query .= " AND Transactions.account_src= :id";
+    $d_query .= " AND Transactions.account_src= :id";
+    $params[":id"] = $id;
+    // Order By desc
+    $t_query .= " ORDER BY Transactions.created DESC";
+    $d_query .= " ORDER BY Transactions.created DESC";
+        
+    global $per_page;
+    $per_page = 10;
+    paginate($t_query,$params, $per_page); //paginate
+
+    $d_query .= " LIMIT :offset, :limit";
+    global $db;
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+    $stmt= $db->prepare($d_query);
+    $results= [];
     try{
-        $stmt->execute();
+        global $total_pages;
+        global $offset;
+        global $page;
+        $params[":offset"]= $offset;
+        $params[":limit"]= $per_page;
+        $stmt->execute($params);
         $results= $stmt->fetchAll();
     }
     catch(PDOException $e){
-        return var_export($e->errorInfo);
+        var_export($e);
     }
-    return $results;
+    $results_transactions = $results;
 }
 ?>
 
-<?php 
-if(isset($_POST["account_id"])){
-    $results_transactions=getTransactionTable($_POST["account_id"]);
-    if (!$results_transactions){
-        echo "<h2>No Transactions Occurred</h2>";
-        die;
-    }
-}
-?>
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-6">
-            <form method="POST" class="border rounded w-50 p-3">
-                <div class="mb-3">
-                    <label for="account_id" class="form-label">Select which account would you like to view:</label>
-                    <select name="account_id" id= "account_id" class="form-select" required>
-                        <option value=""></option>
-                        <?php $results= getAccounts();?>
-                        <?php foreach($results as $key => $value):?>
-                            <option value="<?php se($value["id"]);?>"><?php se($value["account_number"]);?></option>
-                        <?php endforeach;?>		
-                    </select>
-                </div>
-                <div class="input-group mb-3">
-                    <span class="input-group-text">Start</span>
-                    <input type="date" class="form-control" aria-label="Start">
-                    <span class="input-group-text">End</span>
-                    <input type="date" class="form-control">                   
-                </div>
-                <div class="input-group mb-3">
-                    <span class="input-group-text">Type</span> 
-                    <select name="" class="form-select">
-                        <option value=""></option>
-                        <option value="deposit">Deposit</option>
-                        <option value="ext-transfer">External transfer</option>
-                        <option value="transfer">Transfer</option>
-                        <option value="withdraw">Withdraw</option>
-                    </select>
-                </div>
+<div class="container-sm">
+    <div class="row justify-content-center mt-2">
+        <form method="GET" class="border rounded p-3">
+            <input type="hidden" name="account_id" value="<?php se($_GET, "account_id");?>">
+            <div class="input-group mb-3">
+                <span class="input-group-text">Start</span>
+                <input type="date" class="form-control" name="start" aria-label="Start">
+                <span class="input-group-text">End</span>
+                <input type="date" class="form-control" name="end">
+                <span class="input-group-text">Type</span> 
+                <select name="type" class="form-select">
+                    <option value=""></option>
+                    <option value="deposit">Deposit</option>
+                    <option value="ext-transfer">External transfer</option>
+                    <option value="transfer">Transfer</option>
+                    <option value="withdraw">Withdraw</option>
+                </select>
+                <button type="submit" class="btn btn-primary"> Submit</button>
+            </div>
 
-                <div class="mt-3">
-                    <button type="submit" class="btn btn-primary"> Submit</button>
-                </div>            
             </form>
+        </div>
         
-    
-
 
 <?php if(isset($results_transactions)): ?>
-    <?php $account_info= getAccounts($_POST["account_id"]); ?>
-    <table class="table">
-        <thead>
-            <th scope="col">Account number</th>
-            <th scope="col">Type</th>
-            <th scope="col">Balance</th>
-            <th scope="col">Opened</th>
-            <th scope="col">Modified</th>
-        </thead>
-        <tbody>  
-            <tr scope="row">
-                <td> <?php echo $account_info[0]["account_number"];?></td>
-                <td> <?php echo $account_info[0]["account_type"];?></td>
-                <td> <?php echo $account_info[0]["balance"];?></td>
-                <td> <?php echo $account_info[0]["created"];?></td>
-                <td> <?php echo $account_info[0]["modified"];?></td>
-            </tr>
-        </tbody>
-    </table>
-    </div>   
-    <div class="col-6">
-            <h2>Transactions History</h2>
-            <table class="table">
+    <?php $account_info= getAccounts($_GET["account_id"]); ?>
+    <div class="row mb-3">
+        <table class="table table-striped">
+            <p class="h2 p-0">Account Info</p>
+            <thead>
+                <th scope="col">Account number</th>
+                <th scope="col">Type</th>
+                <th scope="col">Balance</th>
+                <th scope="col">Opened</th>
+                <th scope="col">Modified</th>
+            </thead>
+            <tbody>  
+                <tr scope="row">
+                    <td> <?php echo $account_info[0]["account_number"];?></td>
+                    <td> <?php echo $account_info[0]["account_type"];?></td>
+                    <td> <?php echo $account_info[0]["balance"];?></td>
+                    <td> <?php echo $account_info[0]["created"];?></td>
+                    <td> <?php echo $account_info[0]["modified"];?></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    
+    <div class="row">
+            <p class="h2 p-0">Transactions History</p>
+            <?php if ($results_transactions):?>
+            <table class="table table-striped align-middle">
                 <thead>
                     <tr>
                         <th scope="col">Source</th>
@@ -107,6 +130,7 @@ if(isset($_POST["account_id"])){
                         <th scope="col">Change in balance</th>
                         <th scope="col">Expected Total</th>
                         <th scope="col">Memos</th>
+                        <th scope="col">created</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -118,18 +142,20 @@ if(isset($_POST["account_id"])){
                         <td>$<?php echo$value["balance_change"];?></td>
                         <td>$<?php echo$value["expected_total"];?></td>
                         <td><?php echo$value["memo"];?></td>
+                        <td><?php echo$value["created"];?></td>
                     </tr>
                 <?php endforeach?>
                 </tbody>
             </table>
+            <?php else:
+                echo "<center> <h2>No Transactions Occurred</h2> </center>";
+            endif;?>
         </div>
-    </div>       
+</div>       
     
 <?php endif;?>
 </div>
 
-<style>
-    body{
-        background-color: aquamarine;
-    }
-</style>
+<?php include(__DIR__."/../../partials/pagination.php");?>
+
+<?php require_once(__DIR__."/../../partials/flash.php");?>
